02af94834c8d65d98a78e962ed97d12ca0976f70
Kelly Wong <kjw0188@gmail.com>
2012-03-17 01:20:17 -0700

    Changed to lists

---
+++ BarRoomBrawl/GameState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BarRoomBrawl
+{
+    class GameState
+    {
+        public List<GameObject> StaticObjects { get; set; }
+        public List<GameObject> DynamicObjects { get; set; }
+
+        public GameState()
+        {
+            StaticObjects = new List<GameObject>();
+            DynamicObjects = new List<GameObject>();
+        }
+    }
+}
