669122122905f0ab70e752ad1a5a7136487d1e82
tawnos <tawnos@gmail.com>
2012-03-17 02:36:07 -0700

    Updating game object for use by other devs

--- BarRoomBrawl/GameObject.cs
+++ BarRoomBrawl/GameObject.cs
@@ -3,16 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace BarRoomBrawl
 {
-    public class GameObject
+    public class GameObject: IDrawable, IUpdateable
     {
-        public enum Direction { E, SE, S, SW, W, NW, N, NE }
+        public enum Directions { E, SE, S, SW, W, NW, N, NE }
         public int Id { get; set; }
-        public int LocationX { get; set; }
-        public int LocationY { get; set; }
+        public Vector2 Location { get; set; }
+        public float Speed { get; set; }
         public Texture2D Texture { get; set; }
-        public Direction Direction { get; set; }
+        public Directions Direction { get; set; }
+        public BoundingBox BoundingBox { get; set; }
+        
+        // we have no invisible objects
+        public bool Visible
+        {
+            get { return true; }
+        }
+
+        public GameObject(Texture2D texture, Vector2 startLoc, float startSpeed, Directions startDir, int id)
+        {
+            Texture = texture;
+            Location = startLoc;
+            Speed = startSpeed;
+            Direction = startDir;
+            Id = id;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+
+
+
+
+        public int DrawOrder
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<EventArgs> DrawOrderChanged;
+
+        
+
+        public event EventHandler<EventArgs> VisibleChanged;
+
+        public bool Enabled
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<EventArgs> EnabledChanged;
+
+        public int UpdateOrder
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event EventHandler<EventArgs> UpdateOrderChanged;
     }
 }
