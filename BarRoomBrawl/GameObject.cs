51a33cde532e95d9443c4ea1dc4487bd3ee70d98
tawnos <tawnos@gmail.com>
2012-03-17 00:59:32 -0700

    Added gameobject class

---
+++ BarRoomBrawl/GameObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BarRoomBrawl
+{
+    public class GameObject
+    {
+        public enum Direction { E, SE, S, SW, W, NW, N, NE }
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+        public Texture2D Texture { get; set; }
+        public Direction Direction { get; set; }
+    }
+}
