f8d0eff4b788cb6291854f416f13413766ba746b
tawnos <tawnos@gmail.com>
2012-03-17 17:56:25 -0700

    Fixed collision and added some new textures

--- BarRoomBrawl/Map.cs
+++ BarRoomBrawl/Map.cs
@@ -9,25 +9,25 @@ namespace BarRoomBrawl
 {
     public class Map : GameObject
     {
+        Texture2D m_wallTexture;
+        Texture2D m_cornerTexture;
+        Texture2D m_floorTexture;
 
         public Map(Game game)
         {
             m_game = game;
-            
-
         }
 
         public override void LoadContent()
         {
-            m_textureName = "FloorTile";
-            base.LoadContent();
+            m_floorTexture = m_game.Content.Load<Texture2D>("FloorTile");
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
 
-            Rectangle rect = new Rectangle(0, 0, 10 * Texture.Width, 10 * Texture.Height);
-            spriteBatch.Draw(Texture, Vector2.Zero, rect, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+            Rectangle rect = new Rectangle(0, 0, 10 * m_floorTexture.Width, 10 * m_floorTexture.Height);
+            spriteBatch.Draw(m_floorTexture, Vector2.Zero, rect, Color.White, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
                 
         }
 
